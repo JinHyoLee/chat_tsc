39ec04b1482875520a3e6aeb7e538982d7f5bbd1
hyo <jinboy9999@gmail.com>
2023-01-29 00:14:24 +0900

    Changes to be committed:

--- app.js
+++ app.js
@@ -3,25 +3,36 @@ const http = require("http");
 const app = express(); // express 서버 객체
 const path = require("path");
 const server = http.createServer(app);
-const socketIO = require("socket.io");
-const bodyParser_post = require("body-parser");
+const bodyParser = require("body-parser");
+const moment = require("moment");
 
+const socketIO = require("socket.io");
 const io = socketIO(server);
 
-app.set("port", 3000);
-
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "src")));
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// io.on("connection", (socket) => {
-//     socket.on("chatting", (data) => {
-//         const { name, msg } = data;
-//         io.emit("chatting", {
-//             name,
-//             msg,
-//             time: moment(new Date()).format("h:mm A"),
-//         });
-//     });
-// });
+app.post("/api/login", (req, res) => {
+    console.log("POST api");
+    console.log(req.body);
+    let id = req.body.id;
+    let password = req.body.password;
+    if (id === "abc" && password === "123") {
+        res.send({ result: "OK" });
+    } else {
+        res.send({ result: "NO" });
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.on("chatting", (data) => {
+        const { name, msg } = data;
+        io.emit("chatting", {
+            name,
+            msg,
+        });
+    });
+});
 
 server.listen(PORT, () => console.log(`server is running ${PORT}`));
--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ const nav = document.querySelector(".nav");
 const main = document.querySelector(".rooms-main");
 const signIn = document.querySelector("#sign-in");
 const signUp = document.querySelector("#sign-up");
+const header = document.querySelector(".header");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+
+const nickname = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
+
+const socket = io();
 
 const pages = {
     Rooms: "pages/Rooms.html",
@@ -15,7 +26,94 @@ const FriendPages = {
     friends: "pages/Friends_list.html",
 };
 
+function send() {
+    // message 보내기
+    const param = {
+        // name: nickname.value,
+        name: "닉네임",
+        // msg: chatInput.value,
+        msg: "입장",
+    };
+    socket.emit("chatting", param);
+}
+
+socket.on("chatting", (data) => {
+    const { name, msg } = data;
+    console.log(name, msg);
+    // const item = new LiModel(name, msg); // LiModel 인스턴스화
+    // item.makeLi();
+    // displayContainer.scrollTo(0, displayContainer.scrollHeight);
+});
+
+function LiModel(name, msg) {
+    this.name = name;
+    this.msg = msg;
+
+    this.makeLi = () => {
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent" : "received");
+        const dom = `
+         <span class="profile">
+            <span class="user">${this.name} : </span>
+         </span>
+         <span class="message">${this.msg}</span>
+      `;
+        li.innerHTML = dom;
+        chatList.appendChild(li);
+    };
+}
+
+// 채팅방 입장
+async function roomEnter() {
+    header.style.display = "block";
+    const page = await fetch(pages["Rooms"]);
+    const html = await page.text();
+    main.innerHTML = html;
+    const enters = document.querySelectorAll(".enter");
+    console.log(enters);
+    enters.forEach((enter) => {
+        enter.addEventListener("click", async (e) => {
+            const roomMain = document.querySelector(".rooms-main");
+            roomMain.style.display = "none";
+            const roomDetail = document.querySelector(".rooms-detail");
+            roomDetail.style.display = "flex";
+            console.log(e.target.id);
+            send();
+        });
+    });
+}
+
 // 로그인
+signIn.addEventListener("click", async (e) => {
+    const req = {
+        // id: id.value,
+        // password: password.value,
+        id: "abc",
+        password: "123",
+    };
+    fetch(`/api/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req),
+    })
+        .then((res) => {
+            if (res.status != 200) {
+                console.log(res.statusText);
+                throw new Error(res.statusText);
+            }
+            console.log(res);
+            return res.json();
+        })
+        .then(async (data) => {
+            // 성공시 생성된 아이디가 반환됨
+            console.log(data);
+            if (data.result === "OK") {
+                roomEnter();
+            } else {
+                alert("없는 아이디 입니다");
+            }
+        });
+});
 
 // click마다 nav화면전환
 nav.addEventListener("click", async (e) => {
